9d5aaa334d123f5f1acb313edda8f016f0f1ba65
Nouna321 <zaitsadouneogmail.com>
2021-01-13 23:03:06 +0100

    backend

--- smartbrain/src/Components/Register/register.js
+++ smartbrain/src/Components/Register/register.js
@@ -1,47 +1,95 @@
 import React from 'react'
 
-const register = ({ onRouteChange }) => {
-    return (
-        <div className='bg-grey-lighter min-h-screen flex flex-col'>
-            <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
-                <div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
-                    <h1 className='mb-8 text-3xl text-center'>Sign up</h1>
-                    <input type='text' className='block border border-grey-light w-full p-3 rounded mb-4' name='fullname' placeholder='Full Name' />
+class register extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+        }
+    }
 
-                    <input type='text' className='block border border-grey-light w-full p-3 rounded mb-4' name='email' placeholder='Email' />
+    onNameChange = (event) => {
+        this.setState({ name: event.target.value })
+    }
 
-                    <input type='password' className='block border border-grey-light w-full p-3 rounded mb-4' name='password' placeholder='Password' />
-                    <input type='password' className='block border border-grey-light w-full p-3 rounded mb-4' name='confirm_password' placeholder='Confirm Password' />
+    onEmailChange = (event) => {
+        this.setState({ email: event.target.value })
+    }
 
-                    <button
-                        onClick={() => onRouteChange('home')}
-                        type='submit'
-                        className='w-full text-center py-3 rounded bg-green-600 text-white hover:bg-green-dark focus:outline-none my-1'>
-                        Create Account
-                    </button>
+    onPasswordChange = (event) => {
+        this.setState({ password: event.target.value })
+    }
 
-                    <div className='text-center text-sm text-grey-dark mt-4'>
-                        By signing up, you agree to the
-                        <a className='no-underline border-b border-grey-dark text-grey-dark' href='#'>
-                            Terms of Service
+    onSubmitSignIn = () => {
+        fetch('http://localhost:3000/register', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+            }),
+        })
+            .then((response) => response.json())
+            .then((user) => {
+                if (user) {
+                    this.props.loadUser(user)
+                    this.props.onRouteChange('home')
+                }
+            })
+    }
+
+    render() {
+        const { onRouteChange } = this.props
+        return (
+            <div className='bg-grey-lighter min-h-screen flex flex-col'>
+                <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
+                    <div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
+                        <h1 className='mb-8 text-3xl text-center'>Sign up</h1>
+                        <input onChange={this.onNameChange} type='text' className='block border border-grey-light w-full p-3 rounded mb-4' name='name' placeholder='Name' />
+
+                        <input onChange={this.onEmailChange} type='text' className='block border border-grey-light w-full p-3 rounded mb-4' name='email' placeholder='Email' />
+
+                        <input
+                            onChange={this.onPasswordChange}
+                            type='password'
+                            className='block border border-grey-light w-full p-3 rounded mb-4'
+                            name='password'
+                            placeholder='Password'
+                        />
+
+                        <button
+                            onClick={this.onSubmitSignIn}
+                            type='submit'
+                            className='w-full text-center py-3 rounded bg-green-600 text-white hover:bg-green-dark focus:outline-none my-1'>
+                            Create Account
+                        </button>
+
+                        <div className='text-center text-sm text-grey-dark mt-4'>
+                            By signing up, you agree to the
+                            <a className='no-underline border-b border-grey-dark text-grey-dark' href='#'>
+                                Terms of Service
+                            </a>
+                            and
+                            <a className='no-underline border-b border-grey-dark text-grey-dark' href='#'>
+                                Privacy Policy
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className='text-grey-dark mt-6'>
+                        Already have an account?
+                        <a className='no-underline border-b border-blue text-blue' href='../login/'>
+                            Log in
                         </a>
-                        and
-                        <a className='no-underline border-b border-grey-dark text-grey-dark' href='#'>
-                            Privacy Policy
-                        </a>
+                        .
                     </div>
                 </div>
-
-                <div className='text-grey-dark mt-6'>
-                    Already have an account?
-                    <a className='no-underline border-b border-blue text-blue' href='../login/'>
-                        Log in
-                    </a>
-                    .
-                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default register
--- smartbrain/src/Components/SignIn/signin.js
+++ smartbrain/src/Components/SignIn/signin.js
@@ -1,35 +1,76 @@
 import React from 'react'
 
-const signin = ({ onRouteChange }) => {
-    return (
-        <div className='bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4 flex flex-col mr-96 ml-96'>
-            <div className='mb-4'>
-                <label className='block text-grey-darker text-sm font-bold mb-2' htmlFor='username'>
-                    Username
-                </label>
-                <input className='shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker' id='username' type='text' placeholder='Username' />
+class signin extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            signInEmail: '',
+            signInPassword: '',
+        }
+    }
+    onEmailChange = (event) => {
+        this.setState({ signInEmail: event.target.value })
+    }
+
+    onPasswordChange = (event) => {
+        this.setState({ signInPassword: event.target.value })
+    }
+
+    onSubmitSignIn = () => {
+        fetch('http://localhost:3000/signin', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data === 'success') {
+                    this.props.onRouteChange('home')
+                }
+            })
+    }
+
+    render() {
+        return (
+            <div className='bg-white shadow-md rounded px-6 pt-6 pb-8 mb-4 flex flex-col mr-96 ml-96'>
+                <div className='mb-4'>
+                    <label className='block text-grey-darker text-sm font-bold mb-2' htmlFor='email'>
+                        Email
+                    </label>
+                    <input
+                        onChange={this.onEmailChange}
+                        className='shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker'
+                        id='email'
+                        type='email'
+                        placeholder='email'
+                    />
+                </div>
+                <div className='mb-6'>
+                    <label className='block text-grey-darker text-sm font-bold mb-2' htmlFor='password'>
+                        Password
+                    </label>
+                    <input
+                        onChange={this.onPasswordChange}
+                        className='shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3'
+                        id='password'
+                        type='password'
+                        placeholder='******************'
+                    />
+                </div>
+                <div className='flex items-center justify-between'>
+                    <button onClick={this.onSubmitSignIn} className='bg-blue-600 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded' type='button'>
+                        Sign In
+                    </button>
+                    <p onClick={() => this.props.onRouteChange('register')} className='text-grey-darker text-sm font-bold cursor-pointer'>
+                        Register
+                    </p>
+                </div>
             </div>
-            <div className='mb-6'>
-                <label className='block text-grey-darker text-sm font-bold mb-2' htmlFor='password'>
-                    Password
-                </label>
-                <input
-                    className='shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3'
-                    id='password'
-                    type='password'
-                    placeholder='******************'
-                />
-            </div>
-            <div className='flex items-center justify-between'>
-                <button onClick={() => onRouteChange('home')} className='bg-blue-600 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded' type='button'>
-                    Sign In
-                </button>
-                <p onClick={() => onRouteChange('register')} className='text-grey-darker text-sm font-bold cursor-pointer'>
-                    Register
-                </p>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default signin
